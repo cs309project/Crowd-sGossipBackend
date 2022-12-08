f5328160ca6197e06e9e52b6de80382496352476
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-12-08 23:30:59 +0200

    Created user model using mongoose schema

---
+++ src/models/User.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "No email provided"],
+        unique: [true, "Email already exists"],
+    },
+    phone: {
+        type: String,
+        required: false,
+        unique: false,
+    },
+    name: {
+        type: String,
+        required: [true, "No name provided"],
+        unique: false,
+    },
+    password: {
+        type: String,
+        required: [true, "No password provided"],
+        unique: false,
+    },
+    followers: {
+        type: Array,
+        default: [],
+    },
+    following: {
+        type: Array,
+        default: [],
+    },
+    posts: {
+        type: Array,
+        default: [],
+    },
+    unreadPosts: {
+        type: Array,
+        default: [],
+    },
+    chats: {
+        type: Array,
+        default: [],
+    },
+})
+
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
