e48b5ea86294fbe00f0066f7dd92626f55d092b6
meliodas227 <yousefalaa2279@gmail.com>
2022-12-08 13:40:52 +0200

    connect database success and structure done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-
+const mongoose = require('mongoose')
+const src = "mongodb+srv://CodesGossip:code12345678@codesgossip.zsb2lgn.mongodb.net/?retryWrites=true&w=majority"
 const server = app.listen(8000,()=>{
     console.log('app running on port ',8000)
 })
+mongoose.set('strictQuery', false)
+mongoose.connect(src).then(()=>console.log('database connection succesful'))
