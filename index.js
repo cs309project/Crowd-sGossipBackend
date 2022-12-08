3744455eb7743281e64ee4ced66ed126ef1d97f1
meliodas227 <yousefalaa2279@gmail.com>
2022-12-08 13:57:29 +0200

    support usage of es6

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express')
+import express from 'express'
+import mongoose from 'mongoose'
+
 const app = express()
-const mongoose = require('mongoose')
 const src = "mongodb+srv://CodesGossip:code12345678@codesgossip.zsb2lgn.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', false)
+mongoose.connect(src).then(()=>console.log('database connection succesful'))
+
 const server = app.listen(8000,()=>{
     console.log('app running on port ',8000)
 })
-mongoose.set('strictQuery', false)
-mongoose.connect(src).then(()=>console.log('database connection succesful'))
