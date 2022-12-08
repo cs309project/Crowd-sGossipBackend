3448f4f6d07a03f9c289bcb77ce63c675896d24d
meliodas227 <yousefalaa2279@gmail.com>
2022-12-08 15:08:24 +0200

    updated port to be from env

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
 const src = process.env.db
-
+const PORT = process.env.PORT || 5000
 mongoose.set('strictQuery', false)
 mongoose.connect(src).then(()=>console.log('database connection succesful'))
 
-const server = app.listen(8000,()=>{
-    console.log('app running on port ',8000)
+const server = app.listen(PORT,()=>{
+    console.log('app running on port ',PORT)
 })
