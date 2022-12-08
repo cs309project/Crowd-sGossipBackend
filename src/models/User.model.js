e22c65411fe426d2bd8af45dc6d3e802b13912f2
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-12-08 23:35:47 +0200

    Added blocked prop to the user schema

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -41,6 +41,10 @@ const UserSchema = new mongoose.Schema({
         type: Array,
         default: [],
     },
+    blocked: {
+        type: Boolean,
+        default: false,
+    }
 })
 
 module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
