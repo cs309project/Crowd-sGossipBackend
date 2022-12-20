2ba200d13d4226def64f86c75f048686c7c4a35d
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-12-20 23:35:39 +0200

    Added feature: Update comment functionality

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -10,20 +10,20 @@ export const postAdd = async (req, res) => {
     author = mongoose.Types.ObjectId(author)
 
     if (!author || !content) {
-        error = {error: "unknown author or empty content"}
+        error = { error: "unknown author or empty content" }
         console.log('error', error);
         return res.status(401).send(error)
     }
 
-    await addPost({author, content}).then(e => {
+    await addPost({ author, content }).then(e => {
         return res.status(200).send(e)
     }).catch(err => {
         console.log('err', err.message);
-        return res.status(401).send({error: err.message})
+        return res.status(401).send({ error: err.message })
     })
 }
 
-async function addPost({author, content}) {
+async function addPost({ author, content }) {
     const newPost = new Post({
         author,
         content
@@ -33,29 +33,29 @@ async function addPost({author, content}) {
 }
 
 export const postUpdate = async (req, res) => {
-    let {_id , content} = req.body
+    let { _id, content } = req.body
 
     _id = mongoose.Types.ObjectId(_id)
 
-    if (!_id || !content){
-        error = {error: "Id is not provided or empty content"}
+    if (!_id || !content) {
+        error = { error: "Id is not provided or empty content" }
         console.log('error', error);
         return res.status(401).send(error)
     }
-    await updatePost({_id , content}).then(e => {
+    await updatePost({ _id, content }).then(e => {
         return res.status(200).send(e)
     }).catch(err => {
         console.log('err', err.message);
-        return res.status(401).send({error: err.message})
+        return res.status(401).send({ error: err.message })
     })
 }
-export const postDelete = async (req , res) => {
-    let {_id} = req.body
+export const postDelete = async (req, res) => {
+    let { _id } = req.body
 
     _id = mongoose.Types.ObjectId(_id)
 
-    if(!_id){
-        error = {error: "No Id provided"}
+    if (!_id) {
+        error = { error: "No Id provided" }
         console.log('error', error);
         return res.status(401).send(error)
     }
@@ -63,40 +63,67 @@ export const postDelete = async (req , res) => {
         return res.status(200).send(e)
     }).catch(err => {
         console.log('err', err.message);
-        return res.status(401).send({error: err.message})
+        return res.status(401).send({ error: err.message })
     })
 }
 
 export const postGet = async (req, res) => {
-    let {_id} = req.body
+    let { _id } = req.body
     _id = mongoose.Types.ObjectId(_id)
 
     if (!_id) {
-        error = {error: "No id provided"}
+        error = { error: "No id provided" }
         console.log('error', error);
         return res.status(401).send(error)
     }
-    await getPostById({_id}).then(e => {
+    await getPostById({ _id }).then(e => {
         res.status(200).send(e)
     }).catch(err => {
         console.log('err', err.message);
-        return res.status(401).send({error: err.message})
+        return res.status(401).send({ error: err.message })
     })
 }
 
-async function updatePost({_id , content})  {
+async function updatePost({ _id, content }) {
 
-    const editpost = await Post.findByIdAndUpdate({_id} , {content}, {new: true});
+    const editpost = await Post.findByIdAndUpdate({ _id }, { content }, { new: true });
 
     return editpost;
 }
 
-async function deletePost({_id}){
-    const removepost = await Post.findByIdAndDelete({_id});
+async function deletePost({ _id }) {
+    const removepost = await Post.findByIdAndDelete({ _id });
     return removepost;
 }
 
-async function getPostById({_id}) {
-    const post = await Post.findById({_id})
+async function getPostById({ _id }) {
+    const post = await Post.findById({ _id })
     return post
+}
+
+async function updateComment({ _id, commenter, time, updatedComment }) {
+    const post = await Post.findByIdAndUpdate({ _id }, {
+        $set: { 'comments.$[comment].comment': updatedComment }
+    }, { arrayFilters: [{ 'comment.commenter': commenter, 'comment.time': time }] })
+    return post
+}
+
+export const postUpdateComment = async (req, res) => {
+    let { _id, commenter, time, updatedComment } = req.body
+    _id = mongoose.Types.ObjectId(_id)
+    commenter = mongoose.Types.ObjectId(commenter)
+    time = new Date(time)
+
+    if (!commenter || !time) {
+        error = { error: "Couldn't find post" }
+        console.log('error', error);
+        return res.status(400).send(error)
+    }
+
+    await updateComment({ _id, commenter, time, updatedComment }).then(e => {
+        return res.status(200).send(e)
+    }).catch(err => {
+        console.log('err', err.message);
+        return res.status(401).send({ error: err.message })
+    })
 }
