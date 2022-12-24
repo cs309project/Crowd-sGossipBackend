4f736a046513aff68ac5bf6d602c61bebe48c0da
Ahmed_Hussieny <119592011+Ahmed-Hussieny@users.noreply.github.com>
2022-12-24 16:55:01 +0200

    finish register and login

--- src/routes/api/user.route.js
+++ src/routes/api/user.route.js
@@ -2,5 +2,8 @@ import { Router } from "express";
 import * as userController from '../../controllers/user.controller.js'
 const router = Router()
 
+
 router.get('/',userController.get)
+router.post("/register", userController.register);
+router.post("/login", userController.login);
 export default router
