403e108f98f38ccad63218468d53057fa01335ea
Jhany01 <johnhany07@gmail.com>
2022-12-25 21:47:31 +0200

    Add sendMessage , deleteMessage Functions

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -7,6 +7,46 @@ export async function createChat() {
     return newChat
 }
 
-// export const chatAdd = async (req, res) => {
-//     let { user1, user2 } = req.body
-// }
+export async function sendMessage({ _id, sender, message }) {
+
+    const chatDoc = await Chat.findByIdAndUpdate({ _id }, {
+
+        $push: {
+            'conversation': {
+
+                sender,
+                message,
+                time: new Date()
+            }
+        }
+    }, { new: true })
+    return chatDoc
+}
+
+export async function deleteMessage({ _id, sender, time }) {
+    const chatDoc = await Chat.findByIdAndUpdate({ _id }, {
+        $pull: {
+            'conversation': {
+                sender,
+                time
+            }
+        }
+    }, { new: true })
+    return chatDoc
+}
+
+export const getChat = async (req, res) => {
+    let { _id } = req.body
+    _id = mongoose.Types.ObjectId(_id)
+
+    await getChatById(_id).then(e => {
+        return res.status(200).json(e.conversation)
+    }).catch((err) => {
+        console.log("error", err.message);
+        return res.status(401).json({ err: err.message })
+    })
+}
+
+async function getChatById(_id) {
+    return await Chat.findById(_id)
+}
