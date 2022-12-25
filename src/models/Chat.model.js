8f6bc9f2c8d3105f8db4c3f51f30b4671c637f7f
Jhany01 <johnhany07@gmail.com>
2022-12-25 19:11:12 +0200

    Adding the real Schema

--- src/models/Chat.model.js
+++ src/models/Chat.model.js
@@ -1,1 +1,12 @@
- F
+import { Schema, model } from "mongoose";
+
+const ChatSchema = new Schema({
+
+    conversation: {
+
+        type: Array,
+        default: []
+    }
+})
+
+export default model.Chats || model("Chats", ChatSchema);
