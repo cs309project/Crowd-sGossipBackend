83ba07d2a2835e3b6fbf97a8c76bc914b3bf7979
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-12-27 19:15:46 +0200

    changed '/getChat' route to get with id param

--- src/routes/api/chat.route.js
+++ src/routes/api/chat.route.js
@@ -3,6 +3,6 @@ import * as chatController from '../../controllers/chat.controller.js'
 
 const router = Router()
 
-router.get('/getChat' , chatController.getChat)
+router.get('/getChat/:id' , chatController.getChat)
 
 export default router
