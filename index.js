03384ee7bbfd8775a49eb1cbc87be2810563d2c5
Youssef-S-Negm <youssef.negm2001@gmail.com>
2022-12-27 19:13:32 +0200

    fixed socket.io configuration problem

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ import mongoose from 'mongoose'
 import router from './src/routes/index.js'
 import config from './config.js'
 import cors from 'cors'
-import http from 'http'
+import { createServer } from 'http'
 import { Server } from 'socket.io'
 import Chat from './src/models/Chat.model.js'
-import { sendMessage , deleteMessage,} from './src/controllers/chat.controller.js'
+import { sendMessage, deleteMessage, } from './src/controllers/chat.controller.js'
 const app = express()
 app.use(cors())
-const socketConnection = http.Server(app)
+const socketServer = createServer()
+const io = new Server(socketServer, {
+    cors: {
+        origin: `http://localhost:3000`
+    }
+})
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
@@ -21,30 +26,26 @@ const ioPort = 8001;
 mongoose.set('strictQuery', false)
 mongoose.connect(src).then(() => console.log('database connection succesful'))
 
+socketServer.listen(ioPort, () => console.log(`Socket running on ${ioPort}`))
+
 const server = app.listen(PORT, () => {
     console.log('app running on port ', PORT)
 })
 
-
-socketConnection.listen(ioPort, () => console.log(`Socket connected to database on ${ioPort}`))
-const socketIO = new Server(http, {
-    cors: {
-        origin: `http://localhost:${ioPort}`
-    }
-})
-
-socketIO.on('connect', socket => {
+io.on('connect', socket => {
 
     console.log('A client has been connected');
     socket.on('sendMessage', async data => {
         const { _id, sender, message } = data
-        await sendMessage({ _id,  sender, message })
+        await sendMessage({ _id, sender, message })
         socket.emit('chatUpdated')
     })
 
     socket.on('deleteMessage', async data => {
-        const {_id , sender , message} = data
-        await deleteMessage({_id, sender, message})
+        let { _id, sender, time } = data
+        _id = mongoose.Types.ObjectId(_id)
+        time = new Date(time)
+        await deleteMessage({ _id, sender, time })
         socket.emit('chatUpdated')
     })
 })
